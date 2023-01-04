25ca80072f98aef5e1e9b4f9bd86fa60ddb56dd8
rodionov2003 <121936588+rodionov2003@users.noreply.github.com>
2023-01-04 12:42:43 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title> My page </title>
+<link rel="stylesheet" href="mycss.css"
+<link rel="shortcut icon" href="favicon.iso">
+</head>
+<body>
+<div class="mydiv1">
+<?php
+echo "Объем цилиндра с радиусом 5 и высотой 8, равен ";
+echo 3.14*5*5*8
+?>
+</div>
+</body>
+</html>
+
+
+
